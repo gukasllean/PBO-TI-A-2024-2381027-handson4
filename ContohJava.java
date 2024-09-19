cd0961f62c160142df7286800b52604bbfdcbe0f
Lukas <2381027@unai.edu>
2024-09-19 21:25:50 +0700

    Handson4

--- ContohJava.java
+++ ContohJava.java
@@ -1,5 +1,10 @@
 public class ContohJava {
-    public static void main(String[] args) {
+    public static int hitungLuasPersegi(int sisi) {
+        return sisi * sisi;
+    }
 
+    public static void main(String[] args) {
+        int sisi = 5;
+        System.out.println("Luas persegi dari sisi " + sisi + " = " + hitungLuasPersegi(sisi));
     }
 }
